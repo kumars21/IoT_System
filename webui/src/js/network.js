9767649a7ffd38466125bd1e8edd943a3ed9f027
fish47 <fish47@126.com>
2025-05-17 19:15:01 +0800

    webui: network settings modals and callbacks

--- webui/src/js/network.js
+++ webui/src/js/network.js
@@ -1,8 +1,20 @@
 $(function() {
     const $form = $("#network-form-wifi");
+    const $modalSave = $("#network-modal-save");
+    const $modalRevert = $("#network-modal-revert");
+
     $("#network-btn-save").on("click", function() {
         $form.addClass('was-validated');
         if (!$form.get(0).checkValidity())
             return false;
+        new bootstrap.Modal($modalSave).show();
     });
+
+    $("#network-btn-reset").on("click", function() {
+        new bootstrap.Modal($modalRevert).show();
+    });
+
+    $modalSave.find(".btn-primary").on("click", function() {});
+
+    $modalRevert.find(".btn-primary").on("click", function() {});
 });
